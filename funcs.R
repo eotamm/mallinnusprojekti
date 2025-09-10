f9e689bc96ddff654f9c1910c1a4949a03baba9b
Eetu Tammi <eotamm@utu.fi>
2025-09-10 13:58:46 +0300

    Lisää funcs.R

--- funcs.R
+++ funcs.R
@@ -5,7 +5,3 @@ sekunnit_minuteiksi <- function(x, digits = NULL) {
   if (!is.null(digits)) m <- round(m, digits)
   m
 }
-
-sleep_pregnancy$duration_min <- sekunnit_minuteiksi(sleep_pregnancy$duration)
-sleep_pregnancy$awake_min <- sekunnit_minuteiksi(sleep_pregnancy$awake)
-sleep_pregnancy$onset_latency_min <- sekunnit_minuteiksi(sleep_pregnancy$onset_latency)
