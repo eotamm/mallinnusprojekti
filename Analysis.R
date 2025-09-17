00b40f5a3266a7c0009c19a1371a0502114894cc
Eetu Tammi <eotamm@utu.fi>
2025-09-17 10:39:14 +0300

    Uusi analysis.R tiedosto

---
+++ Analysis.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(haven)
+library(janitor)
+library(dplyr)
+
+dir.path <- file.path("./data") # symlink 
+#dir.path <- file.path("../../Seafile/Projektikurssi") # Arille 
+
+
+# Haetaan puhdisteut aineistot
+activity_pregnancy  <- readRDS(file.path(dir.path, "activity_pregnancy.rds"))  %>% as.data.frame() %>% distinct()
+activity_postpartum <- readRDS(file.path(dir.path, "activity_postpartum.rds")) %>% as.data.frame() %>% distinct()
+sleep_pregnancy     <- readRDS(file.path(dir.path, "sleep_pregnancy.rds"))     %>% as.data.frame() %>% distinct()
+sleep_postpartum    <- readRDS(file.path(dir.path, "sleep_postpartum.rds"))    %>% as.data.frame() %>% distinct()
